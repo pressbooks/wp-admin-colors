871920f419bcbd4dab4fdd189ec82c8f89f7febb
Ned Zimmerman <ned@bight.ca>
2017-05-30 17:42:20 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# wp-admin-colors
+
+This package bundles the SCSS files required to compile a WordPress admin color scheme in a npm/yarn-ready package.
+
+## Usage
+
+Add to your project:
+
+```
+yarn add wp-admin-colors --dev
+```
+
+Include in your SCSS file and override some variables:
+
+```
+@import 'node_modules/wp-admin-colors/dist/admin';
+
+...
+```
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var del = require('del');
+var download = require('gulp-download-stream');
+
+var version = '4.7.5';
+
+gulp.task('download', function () {
+  return download([
+    'https://raw.githubusercontent.com/WordPress/WordPress/' + version + '/wp-admin/css/colors/_admin.scss',
+    'https://raw.githubusercontent.com/WordPress/WordPress/' + version + '/wp-admin/css/colors/_variables.scss',
+    'https://raw.githubusercontent.com/WordPress/WordPress/' + version + '/wp-admin/css/colors/_mixins.scss',
+  ])
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', require('del').bind(null, 'dist/'));
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('download');
+});
