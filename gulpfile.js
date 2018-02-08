fee5f2f12fed337225cfdeb1c1f11a75bd0b9e9d
Ned Zimmerman <ned@bight.ca>
2018-02-08 12:54:19 -0400

    Update to 4.9.4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
-let gulp = require('gulp');
-let del = require('del');
-let download = require('gulp-download-stream');
+let gulp = require("gulp");
+let del = require("del");
+let download = require("gulp-download-stream");
 
-let version = '4.9.2';
+let version = "4.9.4";
 
-gulp.task('download', function () {
+gulp.task("download", function() {
   return download([
-    'https://raw.githubusercontent.com/WordPress/WordPress/' + version + '/wp-admin/css/colors/_admin.scss',
-    'https://raw.githubusercontent.com/WordPress/WordPress/' + version + '/wp-admin/css/colors/_variables.scss',
-    'https://raw.githubusercontent.com/WordPress/WordPress/' + version + '/wp-admin/css/colors/_mixins.scss',
-  ])
-  .pipe(gulp.dest('dist/'));
+    "https://raw.githubusercontent.com/WordPress/WordPress/" +
+      version +
+      "/wp-admin/css/colors/_admin.scss",
+    "https://raw.githubusercontent.com/WordPress/WordPress/" +
+      version +
+      "/wp-admin/css/colors/_variables.scss",
+    "https://raw.githubusercontent.com/WordPress/WordPress/" +
+      version +
+      "/wp-admin/css/colors/_mixins.scss"
+  ]).pipe(gulp.dest("dist/"));
 });
 
-gulp.task('clean', require('del').bind(null, 'dist/'));
+gulp.task("clean", require("del").bind(null, "dist/"));
 
-gulp.task('default', ['clean'], function() {
-  gulp.start('download');
+gulp.task("default", ["clean"], function() {
+  gulp.start("download");
 });
