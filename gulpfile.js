6c0b82662d64cf01a5f1871902531e0d40912961
Ned Zimmerman <ned@bight.ca>
2018-04-09 09:57:48 -0300

    4.10.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require("gulp");
 let del = require("del");
 let download = require("gulp-download-stream");
 
-let version = "4.9.4";
+let version = "4.9.5";
 
 gulp.task("download", function() {
   return download([
