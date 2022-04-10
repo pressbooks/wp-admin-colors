c71d199643f6a27e7c8afb0df17a61fe6901f916
Ned Zimmerman <ned@bight.dev>
2022-04-10 20:46:48 -0300

    ci: add auto-update action

---
+++ index.js
@@ -0,0 +1,19 @@
+var semver = require("semver");
+
+let currentVersion, newVersion;
+
+[currentVersion, newVersion] = process.argv.slice(2);
+
+switch (semver.diff(currentVersion, newVersion)) {
+  case "patch":
+    console.log(`fix: add WordPress ${newVersion} support`);
+    break;
+  case "minor":
+    console.log(`feat: add WordPress ${newVersion} support`);
+    break;
+  case "major":
+    console.log(`feat!: add WordPress ${newVersion} support`);
+    break;
+  default:
+    console.log("");
+}
