a101a5412bcaabfe223b12c539d6cd818b715486
Ned Zimmerman <ned@bight.dev>
2022-04-10 21:02:53 -0300

    ci: resolve issues with vesion comparison script

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var semver = require("semver");
 
-let currentVersion, newVersion;
+let v1, v2;
 
-[currentVersion, newVersion] = process.argv.slice(2);
+args = process.argv.slice(2).map((v) => semver.coerce(v));
 
-switch (semver.diff(currentVersion, semver.clean(newVersion))) {
+[v1, v2] = args;
+
+switch (semver.diff(v1, v2)) {
   case "patch":
-    console.log(`fix: add WordPress ${newVersion} support`);
+    console.log(`fix: add WordPress ${v2} support`);
     break;
   case "minor":
-    console.log(`feat: add WordPress ${newVersion} support`);
+    console.log(`feat: add WordPress ${v2} support`);
     break;
   case "major":
-    console.log(`feat!: add WordPress ${newVersion} support`);
+    console.log(`feat!: add WordPress ${v2} support`);
     break;
   default:
     console.log("");
